fb80319e30bbc24dbf8264b2c623c6be6d315752
sushma0101 <117132845+sushma0101@users.noreply.github.com>
2023-04-12 08:40:13 +0545

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)//COnstructor
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()//return type
+        {
+            return RedirectToAction("privacy");
+        }
+
+        public ViewResult Privacy()
+        {
+            return View();
+        }
+        public ContentResult Sushma()
+        {
+            //return View();
+          
+            return Content("the user is Sushma Sapkota.");
+        }
+      
+        public JsonResult Aboutme() {
+            // return RedirectToAction("Index");
+            return Json(new { name = "sushma" }) ;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public FileResult Download()
+        {
+            return File("Myfiles/hello.docx", "text/plain", "hello.docx");
+
+        }
+    }
+}
---
+++ StudentController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        public IActionResult StudentDetail()
+        {
+            StudentModel Sushma = new StudentModel();
+            Sushma.Rollno = 1;
+            Sushma.Name = "Sushma Sapkota";
+            Sushma.Address = "Panauti";
+            Sushma.College = "Nist";
+            Sushma.Phonenum = 98989;
+            return View(Sushma);
+
+        }
+        public IActionResult StudentEdit()
+        {
+            StudentModel Sushma = new StudentModel();
+            Sushma.Rollno = 1;
+            Sushma.Name = "Sushma Sapkota";
+            Sushma.Address = "Panauti";
+            Sushma.College = "Nist";
+            Sushma.Phonenum = 98989;
+            return View(Sushma);
+
+        }
+        public IActionResult Students() {
+        List<StudentModel>Students = new List<StudentModel>();
+            StudentModel Srijit = new StudentModel()
+            {
+                Rollno = 2,
+                Name = "Srijit",
+                Address = "Panauti",
+                College = "Baylor",
+                Phonenum = 989898
+            };
+            StudentModel Safalta = new StudentModel()
+            {
+                Rollno = 20,
+                Name = "Safalta",
+                Address = "Banepa",
+                College = "Nist",
+                Phonenum = 9898
+            };
+            StudentModel Shreeti = new StudentModel()
+            {
+                Rollno = 200,
+                Name = "Shreeti",
+                Address = "Banepa",
+                College = "Nist",
+                Phonenum = 98922
+            };
+            Students.Add(Srijit);
+            Students.Add(Safalta);
+            Students.Add(Shreeti);
+            return View(Students);
+        }
+    }
+}
